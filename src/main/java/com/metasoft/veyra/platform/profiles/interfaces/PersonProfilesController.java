76d6ed87e27b3797930dd3237bfcd437d2e245bc
RenatoCY <renato.calvo2@hotmail.com>
2026-05-27 17:44:39 -0500

    feat(profiles): update profile resources to support file uploads and validation improvements

--- src/main/java/com/metasoft/veyra/platform/profiles/interfaces/PersonProfilesController.java
+++ src/main/java/com/metasoft/veyra/platform/profiles/interfaces/PersonProfilesController.java
@@ -16,6 +16,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -43,7 +44,7 @@ public class PersonProfilesController {
             @ApiResponse(responseCode = "201", description = "Person profile created"),
             @ApiResponse(responseCode = "400", description = "Bad request")
     })
-    public ResponseEntity<PersonProfileResource> createPersonProfile(@ModelAttribute CreatePersonProfileResource resource) {
+    public ResponseEntity<PersonProfileResource> createPersonProfile( @Valid @ModelAttribute CreatePersonProfileResource resource) {
         var createPersonProfileCommand = CreatePersonProfileCommandFromResourceAssembler.toCommandFromResource(resource);
         var personProfile = personProfileCommandService.handle(createPersonProfileCommand);
         if (personProfile.isEmpty()) {
