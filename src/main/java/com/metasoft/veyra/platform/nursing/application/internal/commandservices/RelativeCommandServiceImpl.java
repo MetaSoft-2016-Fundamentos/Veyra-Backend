8ff4e06c7ad7ab0c43fa70861bbe4a9eed6bb5d9
RenatoCY <renato.calvo2@hotmail.com>
2026-05-29 10:46:01 -0500

    feat(nursing): implement handle method for AssignResidentForRelativeCommand in RelativeCommandServiceImpl

--- src/main/java/com/metasoft/veyra/platform/nursing/application/internal/commandservices/RelativeCommandServiceImpl.java
+++ src/main/java/com/metasoft/veyra/platform/nursing/application/internal/commandservices/RelativeCommandServiceImpl.java
@@ -1,19 +1,23 @@
 package com.metasoft.veyra.platform.nursing.application.internal.commandservices;
 import com.metasoft.veyra.platform.nursing.application.internal.outboundservices.acl.ExternalIamService;
 import com.metasoft.veyra.platform.nursing.domain.model.aggregates.Relative;
+import com.metasoft.veyra.platform.nursing.domain.model.commands.AssignResidentForRelativeCommand;
 import com.metasoft.veyra.platform.nursing.domain.model.commands.AssignUserToRelativeCommand;
 import com.metasoft.veyra.platform.nursing.domain.model.commands.CreateRelativeCommand;
 import com.metasoft.veyra.platform.nursing.domain.services.RelativeCommandService;
 import com.metasoft.veyra.platform.nursing.infrastructure.persistence.jpa.repositories.RelativeRepository;
+import com.metasoft.veyra.platform.nursing.infrastructure.persistence.jpa.repositories.ResidentRepository;
 import com.metasoft.veyra.platform.shared.domain.model.valueobjects.EmailAddress;
 import org.springframework.stereotype.Service;
 @Service
 public class RelativeCommandServiceImpl implements RelativeCommandService {
     private final RelativeRepository relativeRepository;
  private  final ExternalIamService externalIamService;
-    public RelativeCommandServiceImpl(RelativeRepository relativeRepository, ExternalIamService externalIamService) {
+ private final ResidentRepository residentRepository;
+    public RelativeCommandServiceImpl(RelativeRepository relativeRepository, ExternalIamService externalIamService, ResidentRepository residentRepository) {
         this.relativeRepository = relativeRepository;
         this.externalIamService = externalIamService;
+        this.residentRepository = residentRepository;
     }
     @Override
     public Long handle(CreateRelativeCommand command) {
@@ -34,6 +38,14 @@ public class RelativeCommandServiceImpl implements RelativeCommandService {
            relative.linkToUser(command.userId());
            relativeRepository.save(relative);
            return relative.getId();
+    }
+
+    @Override
+    public void handle(AssignResidentForRelativeCommand command) {
+        var relative = relativeRepository.findById(command.relativeId()).orElseThrow(() -> new IllegalArgumentException("Relative with id " + command.relativeId() + " not found."));
+        var resident = residentRepository.findById(command.residentId()).orElseThrow(() -> new IllegalArgumentException("Resident with id " + command.residentId() + " not found."));
+        resident.assignedRelativeToResidentCommand(relative);
+
     }
 
 }
