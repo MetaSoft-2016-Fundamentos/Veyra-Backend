e43409a3fe180a6610f1afab95676071c23daa8e
RenatoCY <renato.calvo2@hotmail.com>
2026-05-29 01:18:58 -0500

    feat(nursing): add method to find relative by email address in repository

--- src/main/java/com/metasoft/veyra/platform/nursing/infrastructure/persistence/jpa/repositories/RelativeRepository.java
+++ src/main/java/com/metasoft/veyra/platform/nursing/infrastructure/persistence/jpa/repositories/RelativeRepository.java
@@ -1,9 +1,16 @@
 package com.metasoft.veyra.platform.nursing.infrastructure.persistence.jpa.repositories;
 import com.metasoft.veyra.platform.nursing.domain.model.aggregates.Relative;
 import com.metasoft.veyra.platform.nursing.domain.model.valueobjects.UserId;
+import com.metasoft.veyra.platform.shared.domain.model.valueobjects.EmailAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 @Repository
 public interface RelativeRepository extends JpaRepository<Relative,Long> {
     boolean existsByUserId(UserId userId);
+
+
+    Optional<Relative> findByEmailAddress(EmailAddress emailAddress);
 }
