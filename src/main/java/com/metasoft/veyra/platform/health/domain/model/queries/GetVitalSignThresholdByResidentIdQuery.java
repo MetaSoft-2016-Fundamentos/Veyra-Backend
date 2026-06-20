a97820af0388fed026faa8e3162ab763f3ce9369
Renxoll <112541849+Renxoll@users.noreply.github.com>
2026-06-20 02:17:50 -0500

    feat(health): add method to retrieve user ID by staff ID and update validation thresholds

---
+++ src/main/java/com/metasoft/veyra/platform/health/domain/model/queries/GetVitalSignThresholdByResidentIdQuery.java
@@ -0,0 +1,5 @@
+package com.metasoft.veyra.platform.health.domain.model.queries;
+
+import com.metasoft.veyra.platform.health.domain.model.valueobjects.ResidentId;
+
+public record GetVitalSignThresholdByResidentIdQuery(ResidentId residentId) {}
